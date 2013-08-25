4d44cf6b3b62d7213b94780f7b686217da0c7554
ysadka <ysadka@gmail.com>
2013-08-24 18:54:50 -0700

    branching exercise

---
+++ learn_branching.rb
@@ -0,0 +1,35 @@
+#Instructions
+#Use a new branch to create each method (i.e. addition method is created on the addition branch)
+#Partner 1 builds addition and division methods
+#Partner 2 builds subtration and multiplication methods
+#Work independently and at the same time
+#Use the cylce for pushing  a branch
+
+
+def 
+  
+end
+
+
+
+def 
+  
+end
+
+
+
+def 
+  
+end
+
+
+
+def 
+  
+end
+
+### Driver Code ####
+add(2,3) == 5
+# subtract(6,3) == 3
+# multiply(50, 2) == 100
+# divide(100, 4) == 25
