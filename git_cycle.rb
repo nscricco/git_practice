86b02e5eb4dee7f57740750f6e0d1408912359dc
ysadka <ysadka@gmail.com>
2013-08-24 17:57:54 -0700

    first exercise

---
+++ git_cycle.rb
@@ -0,0 +1,26 @@
+def add(n1, n2)
+  #write a block of code to return the sum 
+  #of 2 numbers then delete these comments
+end
+
+#Exercise 2
+
+# def subtract(n1, n2)
+#     n1  n2 #fix me!!
+#   #fix the block of code to return the difference 
+#   #of 2 numbers then delete these comments
+# end
+
+#Exercise 3
+
+# def multiply(n1, n2)
+#   return n3 
+#   #write a block of code to return the product 
+#   #of 2 numbers then delete these comments
+# end
+
+#Exercise 4
+
+# def divide(n1, n2)
+#   return n1/n2
+# end
