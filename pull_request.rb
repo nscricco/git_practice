d24f111531262c30761ac629034e67f0ccdeab2d
ysadka <ysadka@gmail.com>
2013-09-02 15:40:47 -0700

    pull_request exercise

---
+++ pull_request.rb
@@ -0,0 +1,10 @@
+# Instructions
+# 1) Each of you fork one of your partner's repositories
+# 2) Open the file
+# 3) Create a new branch
+# 4) Refactor a piece of code on the branch and commit it
+# 5) Go to the forked repo on Github
+# 6) Select the proper branches to compare and submit a pull request
+# 7) Repeat steps 3 - 6 but delete a piece of vital code
+# 8) Review the pull requests on your own repo
+# 9) Decide whether or not to accept the pull request 
